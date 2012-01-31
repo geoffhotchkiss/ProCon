8bdcefcb38493164c1d8530431e9b088cdb7d903
Geoff Hotchkiss <geoff@csh.rit.edu>
2012-01-31 15:18:22 -0500

    Changed layout

--- script.js
+++ script.js
@@ -1,43 +1,73 @@
 var i = 1;
-$(document).ready(function() {
-  $("ul").sortable({
-	connectWith: "ul"
-  });
+
+function makeurl(event, ui) {
+	$("#shareurltext").html("Hello world" + i)	
+	i++;
+}
+
+
+$(document).ready(
+	function() {
+		$("#radio").buttonset();
+		$("ul").sortable({connectWith: "ul", 
+											revert: true, 
+											helper:'clone',
+											over: makeurl});
   
-  $("#tasklist").disableSelection();
-  
-  
-  function li_on_click(elem){
-	elem.on('click', function(event){
-		if($(this).attr('to_delete') != "true" || $(this).attr('to_delete') == 'undefined'){
-			$('li[to_delete="true"]').each( function(index) {
-				$(this).attr('to_delete', 'false');
-			});
-			$(this).attr('to_delete', 'true');
-		} else {
-			$(this).attr('to_delete', 'false');
+		$("#proslist").disableSelection();
+		$("#conslist").disableSelection();
+		makeurl(null, null);
+
+
+		function li_on_click(elem) {
+			elem.on('click', 
+				function(event) {
+					if( $(this).attr('to_delete') != "true" || 
+							$(this).attr('to_delete') == 'undefined') {
+						$('li[to_delete="true"]').each( 
+							function(index) {
+								$(this).attr('to_delete', 'false');
+							}
+						);
+						$(this).attr('to_delete', 'true');
+					} 
+					else {
+						$(this).attr('to_delete', 'false');
+					}
+				}
+			);
 		}
-	});
-  }
   
   // initialize click events
-  $('ul.selectable li').each(function(index){
-	li_on_click($(this));
-  });
+		$('ul.selectable li').each(
+			function(index){
+				li_on_click($(this));
+			}
+		);
   
-  $("#add").click(function(event) {
-    event.preventDefault();
-	var new_item = $("<li>" + $("input:text").val() + "</li>");
-	li_on_click(new_item);
-    $("#todo").append(new_item);
-	i++;
-  });
+		$("#add").click(
+			function(event) {
+				event.preventDefault();
+				makeurl(null, null);
+				var inputtext = $("input:text").val();
+				if(inputtext != "") {
+					var new_item = $("<li>" + $("input:text").val() + "</li>");
+					li_on_click(new_item);
+					$("#pros").append(new_item);
+				}
+			}
+		);
   
-  $("#delete").click(function(event) {
-	event.preventDefault();
-	$('li[to_delete="true"]').each( function(index) {
-		$(this).remove();
-	});
-  });
-});
+		$("#delete").click(
+			function(event) {
+				event.preventDefault();
+				$('li[to_delete="true"]').each( 
+					function(index) {
+						$(this).remove();
+					}
+				);
+			}
+		);
+	}
+);
 
