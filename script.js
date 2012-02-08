22d620a45a88ddc252e2dd950b4e0ce8cbbf8a0a
Geoff Hotchkiss <geoff@csh.rit.edu>
2012-02-08 00:35:45 -0500

    Able to grab pros and cons

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var i = 1;
 
+function getarray(kind) {
+	var thearray = new Array();
+	$("#" + kind + "  > li").each( 
+		function(index) {
+			thearray[index] = $(this).text();
+		});
+	return thearray;
+}
+
 function makeurl(event, ui) {
-	$("#shareurltext").html("Hello world" + i)	
+	console.log("Pros: " + getarray("pros"));
+	console.log("Cons: " + getarray("cons"));
+	$("#shareurltext").html("Hello world" + i);
 	i++;
 }
 
@@ -12,7 +23,7 @@ $(document).ready(
 		$("ul").sortable({connectWith: "ul", 
 											revert: true, 
 											helper:'clone',
-											over: makeurl});
+											stop: makeurl});
   
 		$("#proslist").disableSelection();
 		$("#conslist").disableSelection();
@@ -48,13 +59,13 @@ $(document).ready(
 		$("#add").click(
 			function(event) {
 				event.preventDefault();
-				makeurl(null, null);
 				var inputtext = $("input:text").val();
 				if(inputtext != "") {
 					var new_item = $("<li>" + $("input:text").val() + "</li>");
 					li_on_click(new_item);
 					$("#pros").append(new_item);
 				}
+				makeurl(null, null);
 			}
 		);
   
@@ -66,6 +77,7 @@ $(document).ready(
 						$(this).remove();
 					}
 				);
+				makeurl(null, null);
 			}
 		);
 	}
